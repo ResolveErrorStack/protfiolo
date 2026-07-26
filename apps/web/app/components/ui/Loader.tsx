e6f1c230148fc2998583ea6d950c9116164c4baf
ResolveErrorStack <bosshuboffical@gmail.com>
2026-07-26 17:19:46 +0530

    Create Loader.tsx

---
+++ apps/web/app/components/ui/Loader.tsx
@@ -0,0 +1,41 @@
+export default function Loader() {
+  return (
+    <div
+      className="
+        flex
+        flex-col
+        items-center
+        justify-center
+        gap-4
+      "
+    >
+
+      {/* Spinner */}
+      <div
+        className="
+          w-16
+          h-16
+          rounded-full
+          border-4
+          border-cyan-400/30
+          border-t-cyan-400
+          animate-spin
+        "
+      />
+
+
+      {/* Text */}
+      <p
+        className="
+          text-cyan-400
+          text-sm
+          animate-pulse
+        "
+      >
+        Loading...
+      </p>
+
+
+    </div>
+  );
+}
